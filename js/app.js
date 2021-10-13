e57201dec2e498f7be16478fbf2b3579d391a77a
azazmuzaffar <sp18-bcs-035@cuiwah.edu.pk>
2021-10-14 01:45:59 +0500

    Konfidential *Desktop Version* is ready!

--- js/app.js
+++ js/app.js
@@ -3,6 +3,31 @@
 /* >>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<  */
 
 /* >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<  */
+/* >>>>>>>>>>>>>> Navbar <<<<<<<<<<<<<< */
+/* >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<  */
+
+/* >>>>>>>> It will show/hide mobile menu <<<<<<<<< */
+$(document).ready(function () {
+  $(".--menu--").on("click", function () {
+    $(".navbar").toggleClass("active");
+  });
+});
+
+/* >>>>>>>> It will show hide dropdown on click <<<<<<<<< */
+$(document).ready(function () {
+  $(".drop-it").on("click", function () {
+    $(this).toggleClass("active");
+  });
+});
+
+/* >>>>>>>> It will show hide languages <<<<<<<<< */
+$(document).ready(function () {
+  $(".lang").on("click", function () {
+    $(this).toggleClass("active");
+  });
+});
+
+/* >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<  */
 /* >>>>>>>>>>>>> Questions <<<<<<<<<<<< */
 /* >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<  */
 
@@ -14,6 +39,87 @@ $(document).ready(function () {
     $(".--f-a-q-- input.active").removeClass("active");
     $(this).addClass("active");
   });
+});
+
+/* >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<  */
+/* >>>>>> Escorts City Dropdown <<<<<<< */
+/* >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<  */
+
+/* >>>>> Dropdown Menu from Acompanhantes Page <<<<<<*/
+$(".dropdown").click(function () {
+  $(this).attr("tabindex", 1).focus();
+  $(this).toggleClass("active");
+  $(this).find(".dropdown-menu").slideToggle(300);
+});
+$(".dropdown").focusout(function () {
+  $(this).removeClass("active");
+  $(this).find(".dropdown-menu").slideUp(300);
+});
+$(".dropdown .dropdown-menu li").click(function () {
+  $(this).parents(".dropdown").find("span").text($(this).text());
+  $(this).parents(".dropdown").find("input").attr("value", $(this).attr("id"));
+});
+
+/* >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<  */
+/* >>>>>> Contact Form Submission <<<<< */
+/* >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<  */
+
+/* Show confirmation Message */
+$(document).ready(function () {
+  $(".enable-confirmation").on("click", function () {
+    $(".--confirmation-message--").css("display", "block");
+  });
+});
+
+/* Hide confirmation Message */
+$(document).ready(function () {
+  $(".--confirmed--").on("click", function () {
+    $(".--confirmation-message--").css("display", "none");
+  });
+});
+
+/* >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<  */
+/* >>>>> Ads - Make request change <<<< */
+/* >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<  */
+
+/* Show confirmation Message */
+$(document).ready(function () {
+  $(".make-access").on("click", function () {
+    $(".request-accessed").css("display", "block");
+  });
+});
+
+/* Hide confirmation Message */
+$(document).ready(function () {
+  $(".--close-pop-up--").on("click", function () {
+    $(".request-accessed").css("display", "none");
+  });
+});
+
+/* >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<  */
+/* >>>>>>>> All Pages - Login <<<<<<<<< */
+/* >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<  */
+
+/* Show Login Pop Up */
+$(document).ready(function () {
+  $(".make-login").on("click", function () {
+    $(".login-to-konfidential").css("display", "block");
+  });
+});
+
+/* Close Login Pop Up */
+$(document).ready(function () {
+  $(".--close-login--").on("click", function () {
+    $(".login-to-konfidential").css("display", "none");
+  });
+});
+
+/* >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<  */
+/* >>>>>>>>>>>> Read More <<<<<<<<<<<<< */
+/* >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<  */
+
+$(".-read-more").click(function () {
+  $(".-para").toggleClass("active");
 });
 
 /* >>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<  */
